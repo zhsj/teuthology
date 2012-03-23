b7ee143bd7dfa989fb5ac3d63d166d042eadd52a
Mark Nelson <mark.nelson@dreamhost.com>
2012-03-22 21:06:45 -0700

    collectl: Made the task actually run collectl now.
    Signed-off-by: Mark Nelson <nh@metropolis.ceph.dreamhost.com>

--- teuthology/task/collectl.py
+++ teuthology/task/collectl.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
+from cStringIO import StringIO
+
 import contextlib
 import logging
 import os
+import re
+=======
+import contextlib
+import logging
+import os
+>>>>>>> b95a5b65ba4d5a7fb12c913c372ec41b8610e210
 import yaml
 
 from teuthology import contextutil
@@ -8,11 +17,49 @@ from ..orchestra import run
 
 log = logging.getLogger(__name__)
 directory = '/tmp/cephtest/collectl'
+<<<<<<< HEAD
+bin_dir = os.path.join(directory, 'collectl_latest')
+log_dir = '/tmp/cephtest/archive/performance/collectl'
+=======
+>>>>>>> b95a5b65ba4d5a7fb12c913c372ec41b8610e210
 
 @contextlib.contextmanager
 def setup(ctx, config):
     log.info('Setting up collectl')
     for client in config.iterkeys():
+<<<<<<< HEAD
+        cluster = ctx.cluster.only(client)
+
+        for remote in cluster.remotes.iterkeys():
+           proc = remote.run(
+                args=[
+                    'mkdir',
+                    '-p',
+                    directory,
+                    run.Raw('&&'),
+                    'mkdir',
+                    '-p',
+                    log_dir,
+                    run.Raw('&&'),
+                    'wget',
+                    'http://sourceforge.net/projects/collectl/files/latest/download?source=files',
+                    '-O',
+                    os.path.join(directory, 'collectl_latest.tgz'),
+                    run.Raw('&&'),
+                    'tar',
+                    'xvfz',
+                    os.path.join(directory, 'collectl_latest.tgz'),
+                    '-C',
+                    directory,
+                    ],
+                    stdout=StringIO(),
+                )
+           log.info(proc.stdout.getvalue().split('\n', 1)[0])
+           extract_dir = os.path.join(directory, proc.stdout.getvalue().split('\n', 1)[0])
+           for remote in cluster.remotes.iterkeys():
+               proc = remote.run(args=['mv', extract_dir, bin_dir])
+           
+=======
         ctx.cluster.only(client).run(
             args=[
                 'mkdir',
@@ -31,6 +78,7 @@ def setup(ctx, config):
                 directory,
                 ]
             )
+>>>>>>> b95a5b65ba4d5a7fb12c913c372ec41b8610e210
     try:
         yield
     finally:
@@ -38,9 +86,42 @@ def setup(ctx, config):
         for client in config.iterkeys():
             ctx.cluster.only(client).run(args=['rm', '-rf', directory]) 
 
+<<<<<<< HEAD
+@contextlib.contextmanager
+def execute(ctx, config):
+    nodes = {}
+    for client, properties in config.iteritems():
+        if properties is None:
+            properties = {}
+        iteration = properties.get('iteration', 1)
+        piteration = properties.get('piteration', 10)
+
+        cluster = ctx.cluster.only(client)
+        for remote in cluster.remotes.iterkeys():
+            proc = remote.run(
+                args=[
+                    os.path.join(bin_dir, 'collectl.pl'),
+                    '-f',
+                    log_dir,
+                    '-s+YZ',
+                    '-i',
+                    '%s:%s' % (iteration, piteration),
+                    '-F10',
+                    ],
+                wait=False,        
+                )
+            nodes[remote] = proc
+    try:
+        yield
+    finally:
+        for remote in cluster.remotes.iterkeys():
+            log.info('stopping collectl process on %s' % (remote.name))
+            remote.run(args=['pkill', '-f', 'collectl.pl'])
+=======
 #@contextlib.contextmanager
 #def run(ctx, config):
     
+>>>>>>> b95a5b65ba4d5a7fb12c913c372ec41b8610e210
 
 @contextlib.contextmanager
 def task(ctx, config):
@@ -48,10 +129,18 @@ def task(ctx, config):
         config = dict(('client.{id}'.format(id=id_), None)
                   for id_ in teuthology.all_roles_of_type(ctx.cluster, 'client'))
     elif isinstance(config, list):
+<<<<<<< HEAD
+        config = dict.fromkeys(config)
+
+    with contextutil.nested(
+        lambda: setup(ctx=ctx, config=config),
+        lambda: execute(ctx=ctx, config=config),
+=======
         config = dict((name, None) for name in config)
 
     with contextutil.nested(
         lambda: setup(ctx=ctx, config=config),
+>>>>>>> b95a5b65ba4d5a7fb12c913c372ec41b8610e210
         ):
         yield
 
