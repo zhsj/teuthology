cb9dfc7636d45f686cb28ae3f195688b6f20205a
Mark Nelson <mark.nelson@dreamhost.com>
2012-03-30 08:28:38 -0700

    Collectl: Run on all roles by default
    Signed-off-by: Mark Nelson <mark.nelson@dreamhost.com>

--- teuthology/task/collectl.py
+++ teuthology/task/collectl.py
@@ -6,6 +6,7 @@ import os
 import re
 import yaml
 
+from teuthology import misc as teuthology
 from teuthology import contextutil
 from ..orchestra import run 
 
@@ -16,70 +17,70 @@ log_dir = '/tmp/cephtest/archive/performance/collectl'
 
 @contextlib.contextmanager
 def setup(ctx, config):
-    log.info('Setting up collectl')
-    for client in config.iterkeys():
-        cluster = ctx.cluster.only(client)
+    cluster = ctx.cluster
+    if 'clients' in config:
+        l = lambda role: role in config['clients']
+        cluster = ctx.cluster.only(l)
 
-        for remote in cluster.remotes.iterkeys():
-           proc = remote.run(
-                args=[
-                    'mkdir',
-                    '-p',
-                    directory,
-                    run.Raw('&&'),
-                    'mkdir',
-                    '-p',
-                    log_dir,
-                    run.Raw('&&'),
-                    'wget',
-                    'http://sourceforge.net/projects/collectl/files/latest/download?source=files',
-                    '-O',
-                    os.path.join(directory, 'collectl_latest.tgz'),
-                    run.Raw('&&'),
-                    'tar',
-                    'xvfz',
-                    os.path.join(directory, 'collectl_latest.tgz'),
-                    '-C',
-                    directory,
-                    ],
-                    stdout=StringIO(),
-                )
-           log.info(proc.stdout.getvalue().split('\n', 1)[0])
-           extract_dir = os.path.join(directory, proc.stdout.getvalue().split('\n', 1)[0])
-           for remote in cluster.remotes.iterkeys():
-               proc = remote.run(args=['mv', extract_dir, bin_dir])
+    for remote in cluster.remotes.iterkeys():
+        proc = remote.run(
+            args=[
+                'mkdir',
+                '-p',
+                directory,
+                run.Raw('&&'),
+                'mkdir',
+                '-p',
+                log_dir,
+                run.Raw('&&'),
+                'wget',
+                'http://sourceforge.net/projects/collectl/files/latest/download?source=files',
+                '-O',
+                os.path.join(directory, 'collectl_latest.tgz'),
+                run.Raw('&&'),
+                'tar',
+                'xvfz',
+                os.path.join(directory, 'collectl_latest.tgz'),
+                '-C',
+                directory,
+                ],
+                stdout=StringIO(),
+            )
+        log.info(proc.stdout.getvalue().split('\n', 1)[0])
+        extract_dir = os.path.join(directory, proc.stdout.getvalue().split('\n', 1)[0])
+    for remote in cluster.remotes.iterkeys():
+        proc = remote.run(args=['mv', extract_dir, bin_dir])
            
     try:
         yield
     finally:
         log.info('removing collectl')
-        for client in config.iterkeys():
-            ctx.cluster.only(client).run(args=['rm', '-rf', directory]) 
+        cluster.run(args=['rm', '-rf', directory]) 
 
 @contextlib.contextmanager
 def execute(ctx, config):
     nodes = {}
-    for client, properties in config.iteritems():
-        if properties is None:
-            properties = {}
-        iteration = properties.get('iteration', 1)
-        piteration = properties.get('piteration', 10)
+    iteration = config.get('iteration', 1)
+    piteration = config.get('piteration', 10)
+    cluster = ctx.cluster
+    if 'clients' in config:
+        l = lambda role: role in config['clients']
+        cluster = ctx.cluster.only(l)
 
-        cluster = ctx.cluster.only(client)
-        for remote in cluster.remotes.iterkeys():
-            proc = remote.run(
-                args=[
-                    os.path.join(bin_dir, 'collectl.pl'),
-                    '-f',
-                    log_dir,
-                    '-s+YZ',
-                    '-i',
-                    '%s:%s' % (iteration, piteration),
-                    '-F10',
-                    ],
-                wait=False,        
-                )
-            nodes[remote] = proc
+    for remote in cluster.remotes.iterkeys():
+        proc = remote.run(
+            args=[
+                os.path.join(bin_dir, 'collectl.pl'),
+                '-f',
+                log_dir,
+                '-s+YZ',
+                '-i',
+                '%s:%s' % (iteration, piteration),
+                '-F10',
+                ],
+            wait=False,        
+            )
+        nodes[remote] = proc
     try:
         yield
     finally:
@@ -90,11 +91,10 @@ def execute(ctx, config):
 @contextlib.contextmanager
 def task(ctx, config):
     if config is None:
-        config = dict(('client.{id}'.format(id=id_), None)
-                  for id_ in teuthology.all_roles_of_type(ctx.cluster, 'client'))
-    elif isinstance(config, list):
-        config = dict.fromkeys(config)
-
+        config = {}
+    assert isinstance(config, dict), \
+        "please list clients to run on"
+    
     with contextutil.nested(
         lambda: setup(ctx=ctx, config=config),
         lambda: execute(ctx=ctx, config=config),
