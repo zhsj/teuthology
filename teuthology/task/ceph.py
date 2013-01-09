53e674bd66e2050888997f9bd1c08d28325221a2
Sam Lang <sam.lang@inktank.com>
2013-01-09 16:02:42 -0600

    task/mds_thrasher:  New task for thrashing the mds
    Signed-off-by: Sam Lang <sam.lang@inktank.com>

--- teuthology/task/ceph.py
+++ teuthology/task/ceph.py
@@ -41,12 +41,16 @@ class DaemonState(object):
         self.proc = None
         self.log.info('Stopped')
 
-    def restart(self):
+    def restart(self, *args, **kwargs):
         self.log.info('Restarting')
         if self.proc is not None:
             self.log.debug('stopping old one...')
             self.stop()
-        self.proc = self.remote.run(*self.command_args, **self.command_kwargs)
+        cmd_args = list(self.command_args)
+        cmd_args.extend(args)
+        cmd_kwargs = self.command_kwargs
+        cmd_kwargs.update(kwargs)
+        self.proc = self.remote.run(*cmd_args, **cmd_kwargs)
         self.log.info('Started')
 
     def running(self):
@@ -839,7 +843,7 @@ def run_daemon(ctx, config, type_):
         for id_ in teuthology.roles_of_type(roles_for_host, type_):
             name = '%s.%s' % (type_, id_)
 
-            if not id_.endswith('-s'):
+            if not (id_.endswith('-s') or id_[:-1].endswith('-s-r')):
                 num_active += 1
 
             run_cmd = [
@@ -868,6 +872,10 @@ def run_daemon(ctx, config, type_):
                 run_cmd.extend([ 'env', 'CPUPROFILE=%s' % profile_path ])
 
             run_cmd.extend(run_cmd_tail)
+
+            if id_[:-1].endswith('-s-r'):
+                run_cmd.extend(['--hot-standby', id_[-1]])
+
             ctx.daemons.add_daemon(remote, type_, id_,
                                    args=run_cmd,
                                    logger=log.getChild(name),
--- teuthology/task/ceph_manager.py
+++ teuthology/task/ceph_manager.py
@@ -605,3 +605,44 @@ class CephManager:
                     'failed to reach quorum size %d before timeout expired' % size
             time.sleep(3)
         self.log("quorum is size %d" % size)
+
+
+    ## metadata servers
+
+    def kill_mds(self, mds):
+        self.ctx.daemons.get_daemon('mds', mds).stop()
+
+    def kill_mds_by_rank(self, rank):
+        status = self.get_mds_status_by_rank(rank)
+        self.ctx.daemons.get_daemon('mds', status['name']).stop()
+
+    def revive_mds(self, mds, standby_for_rank=None):
+        args = []
+        if standby_for_rank:
+          args.extend(['--hot-standby', standby_for_rank])
+        self.ctx.daemons.get_daemon('mds', mds).restart(*args)
+
+    def revive_mds_by_rank(self, rank, standby_for_rank=None):
+        args = []
+        if standby_for_rank:
+          args.extend(['--hot-standby', standby_for_rank])
+        status = self.get_mds_status_by_rank(rank)
+        self.ctx.daemons.get_daemon('mds', status['name']).restart(*args)
+
+    def get_mds_status(self, mds):
+        out = self.raw_cluster_cmd('mds', 'dump', '--format=json')
+        j = json.loads(' '.join(out.splitlines()[1:]))
+        # collate; for dup ids, larger gid wins.
+        for info in j['info'].itervalues():
+          if info['name'] == mds:
+              return info
+        return None
+
+    def get_mds_status_by_rank(self, rank):
+        out = self.raw_cluster_cmd('mds', 'dump', '--format=json')
+        j = json.loads(' '.join(out.splitlines()[1:]))
+        # collate; for dup ids, larger gid wins.
+        for info in j['info'].itervalues():
+          if info['rank'] == rank:
+              return info
+        return None
