c87a25f5daf152a711b8817dc707528ec5df0682
Warren Usui <warren.usui@inktank.com>
2015-11-12 11:33:14 -0800

    -x now excludes librados2-devel and librbd1-devel Made sure that librado2, librbd1 and libcephfs1 are not installed before doing the ceph installations.

--- teuthology/task/install.py
+++ teuthology/task/install.py
@@ -261,8 +261,7 @@ def _update_rpm_package_list_and_install(ctx, remote, rpm, config):
         args=[
             'sudo', 'yum', 'clean', 'all',
         ])
-    #remote.run(args = ['sudo', 'yum', 'install', 'epel-release', '-y',])
-    #remote.run(args=['sudo', 'yum', 'erase', 'yum-plugin-priorities', '-y'])
+    remote.run(args = ['sudo', 'yum', 'remove', 'librados2', 'libcephfs1', '-y',])
     ldir = _get_local_dir(config, remote)
     for cpack in rpm:
         pkg = None
@@ -275,16 +274,16 @@ def _update_rpm_package_list_and_install(ctx, remote, rpm, config):
                 args = ['if', 'test', '-e',
                         run.Raw(pkg), run.Raw(';'), 'then',
                         'sudo', 'yum', 'remove', pkg, '-y', run.Raw(';'),
-                        'sudo', 'yum', 'install', pkg, '-y', '-x', 'librbd-devel', '-x', 'librados-devel',
+                        'sudo', 'yum', 'install', pkg, '-y', '-x', 'librbd1-devel', '-x', 'librados2-devel',
                         run.Raw(';'), 'fi']
             )
         if pkg is None:
-            remote.run(args = ['sudo', 'yum', 'install', cpack, '-y', '-x', 'librbd-devel', '-x', 'librados-devel',])
+            remote.run(args = ['sudo', 'yum', 'install', cpack, '-y', '-x', 'librbd1-devel', '-x', 'librados2-devel',])
         else:
             remote.run(
                 args = ['if', 'test', run.Raw('!'), '-e',
                         run.Raw(pkg), run.Raw(';'), 'then',
-                        'sudo', 'yum', 'install', cpack, '-y', '-x', 'librbd-devel', '-x', 'librados-devel',
+                        'sudo', 'yum', 'install', cpack, '-y', '-x', 'librbd1-devel', '-x', 'librados2-devel',
                         run.Raw(';'), 'fi'])
 
 
