dae5c4459f328b1b4cc8944756a6e9dd3f01ce3c
Zack Cerza <zack@redhat.com>
2015-11-16 17:31:25 -0700

    Drop beanstalkd

--- teuthology/schedule.py
+++ teuthology/schedule.py
@@ -1,7 +1,6 @@
 import pprint
 import yaml
 
-import teuthology.beanstalk
 from teuthology.misc import get_user, merge_configs
 from teuthology import report
 
@@ -69,18 +68,8 @@ def schedule_job(job_config, num=1):
     :param num:      The number of times to schedule the job
     """
     num = int(num)
-    job = yaml.safe_dump(job_config)
-    tube = job_config.pop('tube')
-    beanstalk = teuthology.beanstalk.connect()
-    beanstalk.use(tube)
     while num > 0:
-        jid = beanstalk.put(
-            job,
-            ttr=60 * 60 * 24,
-            priority=job_config['priority'],
-        )
-        print 'Job scheduled with name {name} and ID {jid}'.format(
-            name=job_config['name'], jid=jid)
-        job_config['job_id'] = str(jid)
-        report.try_push_job_info(job_config, dict(status='queued'))
+        job_id = report.try_push_job_info(job_config, dict(status='queued'))
+        print 'Job scheduled with name {0} and ID {1}'.format(
+            job_config['name'], job_id)
         num -= 1
